d0d4cdbd1c94aa129bb15de8313df5b74878ab0e
Ashish Nallana <ashishnallana@gmail.com>
2023-07-21 08:26:48 +0530

    updating cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 
@@ -10,7 +11,20 @@ const port = process.env.PORT || 3001;
 // connecting to db
 require("./db/connection");
 
+const allowedOrigins = ["https://mern-todo-844aa.web.app"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 // linking express router
 app.use(require("./router/route"));
